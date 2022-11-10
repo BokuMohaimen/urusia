c24d737462e20f9f324cb78a76700878e5ecc931
BokuMohaimen <you10217114514@gmail.com>
2022-11-10 21:40:48 +0900

    Initial commit

---
+++ src/main/java/urusia/urusia/Urusia.java
@@ -0,0 +1,34 @@
+package urusia.urusia;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Urusia extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (command.getName().equalsIgnoreCase("urusai")) {
+
+            for (int i = 0; i <= 2147483647; i++) {
+                sender.sendMessage("うるｒしあ「");
+            }
+            return true;
+
+        }
+
+        return false;
+    }
+}
